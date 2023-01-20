aaf02221ab776e1485c7e0a8c308c338fd0e1f71
gurizenit <gurizenit@gmail.com>
2023-01-20 19:58:32 -0300

    update: improvements in messageCreate.js and openai.js

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -1,45 +1,33 @@
 const openai = require("../Structures/openai.js");
-const AI = { name: "Syalis" };
 
 module.exports = {
   once: false,
   async execute(message) {
     const { client, author } = message;
+    const aiName = client.user.username;
 
     if (!message.mentions.has(client.user.id)) return;
-
-    openai.body.stop[0] = ` ${author.username}:`;
     message.channel.sendTyping();
+    openai.body.stop[0] = ` ${author.username}:`;
 
     const mention = "<@" + client.user.id + ">";
     const userMessage = message.content.replace(mention, "");
 
     openai.push(`${author.username}: ${userMessage}`);
 
-    const response = await openai.execute();
-
-    if (!response) return errorDetected("response!");
-
-    const data = await response.json();
-    process.data = data;
-
-    if (!data.choices) return errorDetected("choices!");
-
+    const data = await openai.execute();
     const aiMessage = data.choices[0].text;
-    console.log(data.choices)
 
-    if (!aiMessage.includes(`${AI.name}:`)) {
-      openai.push(`${AI.name}: ${aiMessage}`);
-    } else {
-      openai.push(aiMessage);
+    if (!aiMessage.startsWith("Error:")) {
+      if (!aiMessage.includes(`${aiName}:`)) {
+        openai.push(`${aiName}: ${aiMessage}`);
+      } else {
+        openai.push(aiMessage);
+      }
     }
-    if (!aiMessage.length) return;
-
-    message.reply(aiMessage.replace(`${AI.name}: `, ""));
-
-    console.log(openai.body.prompt);
-    function errorDetected(error) {
-      return message.reply("> Theres a error in:" + error);
-    }
+   
+    if (!aiMessage.trim().replace(`${aiName}: `, "").length) return;
+    
+    message.reply(aiMessage.replace(`${aiName}: `, ""));
   },
 };
--- src/Structures/openai.js
+++ src/Structures/openai.js
@@ -5,15 +5,19 @@ const AI_NAME = "Syalis";
 const url = `https://api.openai.com/v1/engines/${model}/completions`;
 
 module.exports = {
-  cache: [
+  context: [
     `I am an AI assist called ${AI_NAME}, that will help users!`,
+    "Programing codes will be sent ike this:  ```language_name\ncode here\n```",
+    "language_name should be in lowercase!",
+    "Your messages Must be 2000 or fewer in char length. never pass that!",
     "The name of the users is on the start of each message",
     "USER_NAME: Hello I'm a user!",
-    `${AI_NAME}: Nice to meet you USER_NAME!`,
+    `${AI_NAME}: Nice to meet you USER_NAME!\n`,
   ],
+  cache: [],
   body: {
     prompt: "",
-    temperature: 0.9,
+    temperature: 0,
     max_tokens: 2048,
     top_p: 1,
     frequency_penalty: 0,
@@ -21,7 +25,8 @@ module.exports = {
     stop: [" USER_NAME:", ` ${AI_NAME}:`],
   },
   async execute() {
-    this.body.prompt = this.cache.join("\n");
+    const { context, cache } = this;
+    this.body.prompt = context.join("\n") + cache.join("\n");
 
     const response = await fetch(url, {
       method: "POST",
@@ -32,7 +37,20 @@ module.exports = {
       body: JSON.stringify(this.body),
     }).catch(() => {});
 
-    return response;
+    const data = await response.json();
+
+    if (data.error)
+      return {
+        choices: [{ text: "Error: " + data.error.message }],
+      };
+
+    if (data.usage) {
+      if (data.usage.total_tokens >= 1000) {
+        for (let i = 0; i < 3; i++) this.cache.shift();
+      }
+    }
+
+    return data;
   },
   push(args) {
     this.cache.push(`${args} <|endoftext|>`);
