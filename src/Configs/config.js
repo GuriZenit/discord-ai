0096a06c378f35c9efae69e483be6a12ce46e3ea
gurizenit <gurizenit@gmail.com>
2023-01-20 21:49:21 -0300

    update: moved contex to config.js

--- src/Configs/config.js
+++ src/Configs/config.js
@@ -7,5 +7,13 @@ module.exports = {
   openai: {
     key: process.env.OPENAI_API_KEY,
     model: "text-davinci-003",
+    context: [
+      `I am an AI assist called %AI_NAME, that will help users!`,
+      "Programing codes will be sent ike this:  ```language_name\ncode here\n```",
+      "language_name should be in lowercase!",
+      "The name of the users is on the start of each message",
+      "%USER_NAME: Hello I'm a user!",
+      `%AI_NAME: Nice to meet you %USER_NAME!\n`,
+    ],
   },
 };
